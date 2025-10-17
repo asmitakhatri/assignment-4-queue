b4889ee96baa1815e99914f88ffe366dd7dbce10
asmitakhatri <aasmita_be24@thapar.edu>
2025-10-17 14:59:58 +0530

    Q3queue.cpp

--- Q3queue.cpp
+++ Q3queue.cpp
@@ -2,34 +2,51 @@
 #include <queue>
 using namespace std;
 
-int main() {
-    queue<int> q, firstHalf;
-    int n, val;
-
-    cout << "Enter even number of elements: ";
-    cin >> n;
-
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> val;
-        q.push(val);
+void interleaveQueue(queue<int>& q) {
+    int n = q.size();
+    if (n % 2 != 0) {
+        cout << "Queue has odd number of elements, cannot interleave perfectly." << endl;
+        return;
     }
 
+    queue<int> firstHalf;
+
     
-    for (int i = 0; i < n/2; i++) {
+    for (int i = 0; i < n / 2; i++) {
         firstHalf.push(q.front());
         q.pop();
     }
 
-
+    
     while (!firstHalf.empty()) {
-        cout << firstHalf.front() << " ";
+        q.push(firstHalf.front());
         firstHalf.pop();
 
+        q.push(q.front());
+        q.pop();
+    }
+}
+
+int main() {
+    queue<int> q;
+
+
+    q.push(4);
+    q.push(7);
+    q.push(11);
+    q.push(20);
+    q.push(5);
+    q.push(9);
+
+    interleaveQueue(q);
+
+  
+    cout << "Interleaved Queue: ";
+    while (!q.empty()) {
         cout << q.front() << " ";
         q.pop();
     }
-
     cout << endl;
+
     return 0;
 }
